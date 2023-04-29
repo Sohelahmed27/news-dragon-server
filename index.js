ef6b9d8db99478ddabc59dfc5b34b9272a1b40fa
Sohelahmed27 <sohel.gebcu@gmail.com>
2023-04-29 07:33:05 +0600

    news-server

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const cors = require('cors');
 const app = express();
 const port =  5000;
 
-const catagories = require('./Data/catagories.json')
+const catagories = require('./Data/catagories.json');
+const news = require('./Data/news.json');
 
 app.use(cors());
 
@@ -13,6 +14,28 @@ app.get('/', (req, res) => {
 
 app.get('/catagories', (req, res) => {
   res.send(catagories)
+})
+
+app.get('/news', (req, res) => {
+  res.send(news)
+})
+app.get('/news/:id', (req, res) => {
+  const id = req.params.id;
+  console.log(id)
+  const selectedNews = news.find(news => news._id === id);
+  res.send(selectedNews);
+})
+
+app.get('/categories/:id', (req, res) => {
+   const id = parseInt(req.params.id);
+   console.log(id);
+   if(id === 0){
+      res.send(news)
+   }
+   else{
+    const categoryNews = news.filter(news =>parseInt(news.category_id) === id)
+    res.send(categoryNews);
+   }  
 })
 
 app.listen(port, ()=>{
